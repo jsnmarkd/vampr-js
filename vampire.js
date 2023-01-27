483e4fe775e271b674cc2d21b0d8e55e535b574d
Jason Divinagracia <jmarkd04@gmail.com>
2023-01-27 17:38:11 -0500

    Implemented half of stretch function

--- vampire.js
+++ vampire.js
@@ -48,7 +48,14 @@ class Vampire {
   // * when comparing Ansel and Sarah, Ansel is the closest common anscestor.
   // * when comparing Ansel and Andrew, Ansel is the closest common anscestor.
   closestCommonAncestor(vampire) {
-    
+    let currentVampire = this;
+    if (!vampire.creator) {
+      return vampire;
+    }
+    if (currentVampire.creator === vampire) {
+      return vampire;
+    }
+    return currentVampire;
   }
 }
 
